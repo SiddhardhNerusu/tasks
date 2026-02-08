78967289117fd6d5141b022ff90b87094f3e6e22
Siddhardh Nerusu <siddhardhnerusu@gmail.com>
2026-02-08 19:47:32 +0000

    Remove broken vercel runtime config and normalize health handler

--- api/health.js
+++ api/health.js
@@ -1,3 +1,9 @@
-export default function handler(req, res) {
-  res.status(200).json({ ok: true });
+function handler(_req, res) {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "application/json; charset=utf-8");
+  res.setHeader("Cache-Control", "no-store");
+  res.end("{\"ok\":true}\n");
 }
+
+module.exports = handler;
+module.exports.default = handler;
